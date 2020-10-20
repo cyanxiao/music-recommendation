366265c7f51869f1c168ab09016f25a357dd29f8
RealTonyShaw <tonyshaw123@hotmail.com>
2020-10-20 16:41:13 +0800

    feat: update get_x_in_u() to adapt it to matrices with nan

--- mat_process/incremental_calculation.py
+++ mat_process/incremental_calculation.py
@@ -15,8 +15,25 @@ def get_x_in_u(u_single_line_mat: np.ndarray, v_mat: np.ndarray, user_preference
     for i in range(u_single_line_mat.shape[0]):
         new_u_single_line_mat[i] = 0
         tmp_user_preference_single_line = np.dot(new_u_single_line_mat, v_mat)
-        other_sum = np.sum(user_preference_single_line - tmp_user_preference_single_line)
-        coefficient_x = np.sum(v_mat[i, :])
+        other_sum = np.nansum(user_preference_single_line - tmp_user_preference_single_line)
+        # coefficient_x = np.sum(v_mat[i, :])
+        # TODO: 需要进一步检查生成矩阵
+        coefficient_x = _get_coefficient_x(v_mat[i, :], user_preference_single_line)
+        # print('coefficient_x', coefficient_x)
+        # print('_get_coefficient_x', _get_coefficient_x(v_mat[i, :], user_preference_single_line))
         x = other_sum / coefficient_x
         new_u_single_line_mat[i] = x
     return new_u_single_line_mat
+
+
+def _get_coefficient_x(v_single_line_mat: np.ndarray, user_preference_single_line: np.ndarray) -> float:
+    """
+    获取在 user_preference_single_line 中有 NaN 时未定数的系数（仅在 get_x_in_u() 中使用）
+    """
+    nan_positions = np.argwhere(np.isnan(user_preference_single_line))
+    coefficient = 0.0
+    for i in range(v_single_line_mat.shape[0]):
+        if i in nan_positions:
+            continue
+        coefficient += v_single_line_mat[i]
+    return coefficient
