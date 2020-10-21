9bb050dff7a2da69e6c0de581351dc90e84a830e
RealTonyShaw <tonyshaw123@hotmail.com>
2020-10-21 14:42:29 +0800

    feat: add another incremental calculation method

--- mat_process/incremental_calculation.py
+++ mat_process/incremental_calculation.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-
-def get_x_in_u(u_single_line_mat: np.ndarray, v_mat: np.ndarray, user_preference_single_line: np.ndarray) -> np.ndarray:
+"""
+增量计算方法 1
+Deprecated
+"""
+def _get_x_in_u(u_single_line_mat: np.ndarray, v_mat: np.ndarray,
+                user_preference_single_line: np.ndarray) -> np.ndarray:
     """
     对 U 矩阵的某一行进行计算，得到一行的增量计算结果（解方程）
     :param u_single_line_mat: U 的某一行
@@ -16,11 +20,8 @@ def get_x_in_u(u_single_line_mat: np.ndarray, v_mat: np.ndarray, user_preference
         new_u_single_line_mat[i] = 0
         tmp_user_preference_single_line = np.dot(new_u_single_line_mat, v_mat)
         other_sum = np.nansum(user_preference_single_line - tmp_user_preference_single_line)
-        # coefficient_x = np.sum(v_mat[i, :])
         # TODO: 需要进一步检查生成矩阵
         coefficient_x = _get_coefficient_x(v_mat[i, :], user_preference_single_line)
-        # print('coefficient_x', coefficient_x)
-        # print('_get_coefficient_x', _get_coefficient_x(v_mat[i, :], user_preference_single_line))
         x = other_sum / coefficient_x
         new_u_single_line_mat[i] = x
     return new_u_single_line_mat
@@ -28,7 +29,7 @@ def get_x_in_u(u_single_line_mat: np.ndarray, v_mat: np.ndarray, user_preference
 
 def _get_coefficient_x(v_single_line_mat: np.ndarray, user_preference_single_line: np.ndarray) -> float:
     """
-    获取在 user_preference_single_line 中有 NaN 时未定数的系数（仅在 get_x_in_u() 中使用）
+    获取在 user_preference_single_line 中有 NaN 时未定数的系数（仅在 _get_x_in_u() 中使用）
     """
     nan_positions = np.argwhere(np.isnan(user_preference_single_line))
     coefficient = 0.0
@@ -37,3 +38,54 @@ def _get_coefficient_x(v_single_line_mat: np.ndarray, user_preference_single_lin
             continue
         coefficient += v_single_line_mat[i]
     return coefficient
+
+
+"""
+增量计算方法 2
+"""
+def get_x(u: np.ndarray, v: np.ndarray, m: np.ndarray, r: int, s: int) -> int:
+    """
+    获取 U 矩阵在 (r, s) 位置的元素的值，使 M 与 UV 间的 RMSE 最小
+    :param u: U
+    :param v: V
+    :param m: M
+    :param r: r
+    :param s: s
+    :return: U(r, s) 的值
+    """
+    assert u.shape[1] == v.shape[0], 'u & v can not be multiplied.'
+    assert u.shape[0] == m.shape[0] and v.shape[1] == m.shape[1], 'the shape of m/u/v needs to be revised.'
+    numerator = 0.0
+    denominator = 0.0
+    for j in range(v.shape[1]):
+        sum_of_u_v = np.dot(u[r, :], v[:, j]) - u[r, s] * v[s, j]
+        single_numerator = v[s, j] * (m[r, j] - sum_of_u_v)
+        if np.isnan(m[r, j]):
+            continue
+        numerator += single_numerator
+        denominator += v[s, j] ** 2
+    return numerator / denominator
+
+
+def get_y(u: np.ndarray, v: np.ndarray, m: np.ndarray, r: int, s: int) -> int:
+    """
+
+    :param u:
+    :param v:
+    :param m:
+    :param r:
+    :param s:
+    :return:
+    """
+    assert u.shape[1] == v.shape[0], 'u & v can not be multiplied.'
+    assert u.shape[0] == m.shape[0] and v.shape[1] == m.shape[1], 'the shape of m/u/v needs to be revised.'
+    numerator = 0.0
+    denominator = 0.0
+    for i in range(u.shape[0]):
+        sum_of_u_v = np.dot(u[i, :], v[:, s]) - u[i, r] * v[r, s]
+        single_numerator = u[i, r] * (m[i, s] - sum_of_u_v)
+        if np.isnan(m[i, s]):
+            continue
+        numerator += single_numerator
+        denominator += u[i, r] ** 2
+    return numerator / denominator
