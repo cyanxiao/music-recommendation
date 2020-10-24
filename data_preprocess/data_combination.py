4caccb18f77f124e68bbea3d68b160c1dd674d46
RealTonyShaw <tonyshaw123@hotmail.com>
2020-10-24 23:23:37 +0800

    feat: basic merge (not tested)

--- data_preprocess/data_combination.py
+++ data_preprocess/data_combination.py
@@ -3,14 +3,45 @@ import pandas as pd
 import dask.dataframe as dd
 
 
-def merge_duplicate(user_artist_data: str, artist_alias: str):
-    user_artist = dd.read_csv(user_artist_data, header=None, delimiter=' ', dtype={0: 'int', 1: 'int', 2: 'int'})
+def merge(result_data: str, merged_data: str):
+    user_artist = pd.read_csv(result_data, header=None, delimiter=' ', dtype={0: 'int', 1: 'int', 2: 'int'})
     user_artist.columns = ['user', 'artist', 'num']
-    alias = dd.read_csv(artist_alias, header=None, delimiter='\t')
-    alias = alias.fillna(-1).astype('int')  # 忽略缺少某列的情况
-    alias.columns = ['alias', 'artist']
-    mid = dd.merge(user_artist, alias, left_on='artist', right_on='artist', how='left')
-    result = dd.merge(mid, user_artist, left_on='alias', right_on='artist', how='left')
-    result = result.compute()
-    print(result.iloc[0, :])
+    user_artist.groupby(['user', 'artist']).sum()
+    user_artist.to_csv(merged_data)
 
+
+def get_alias_to_name(artist_alias: str) -> dict:
+    alias = open(artist_alias)
+    line = alias.readline()
+    alias_to_artist = dict()
+    while line:
+        try:
+            line_content = line.strip().split('\t')
+            alias_to_artist[int(line_content[0])] = int(line_content[1])
+            line = alias.readline()
+        except:
+            line = alias.readline()
+    alias.close()
+    return alias_to_artist
+
+
+def replace_alias(artist_alias: str, user_artist_data: str, result_data: str):
+    alias_to_artist = get_alias_to_name(artist_alias)
+    user_artist = open(user_artist_data)
+    lines = []
+    line = user_artist.readline()
+    while line:
+        line_content = line.split(' ')
+        if line_content[1] in alias_to_artist.keys():
+            line_content[1] = alias_to_artist[line_content[1]]
+            line_content = " ".join(str(x) for x in line_content)
+            lines.append(line_content)
+            line = user_artist.readline()
+            continue
+        lines.append(line)
+        line = user_artist.readline()
+    user_artist.close()
+    result = open(result_data, 'a')
+    for line in lines:
+        result.write(line)
+    result.close()
