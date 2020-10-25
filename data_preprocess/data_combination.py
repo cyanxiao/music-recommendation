3d20727bb08fc9809de3ef50607432de5a8de262
RealTonyShaw <tonyshaw123@hotmail.com>
2020-10-25 17:26:19 +0800

    doc: add merge() spec

--- data_preprocess/data_combination.py
+++ data_preprocess/data_combination.py
@@ -4,13 +4,27 @@ import dask.dataframe as dd
 
 
 def merge(artist_alias: str, user_artist_data: str, result_data: str, merged_data: str):
-    replace_alias(artist_alias, user_artist_data, result_data)
+    """
+    导出一个合并了 alias 和本名（全部根据 artist_alias 替换成本名）的艺术家列表，形如：
+    user,artist,num
+    90,24,1
+    90,46,4
+    90,121,1
+    ......
+    其中不存在 (user, artist) 的重复项，不可能同时存在  (user, artist) 相同的两行，不可能既有 (90,24,1) 又有 (90,24,2)
+    :param artist_alias: artist_alias.txt 路径
+    :param user_artist_data: user_artist_data.txt 路径
+    :param result_data: 一个中间结果的导出路径，存在 (user, artist) 的重复项
+    :param merged_data: 最终结果的导出路径，不存在 (user, artist) 的重复项
+    :return:
+    """
+    _replace_alias(artist_alias, user_artist_data, result_data)
     user_artist = pd.read_csv(result_data, header=None, index_col=None, delimiter=' ', dtype={0: 'int', 1: 'int', 2: 'int'})
     user_artist.columns = ['user', 'artist', 'num']
     user_artist.groupby(['user', 'artist']).sum().to_csv(merged_data)
 
 
-def get_alias_to_name(artist_alias: str) -> dict:
+def _get_alias_to_name(artist_alias: str) -> dict:
     alias = open(artist_alias)
     line = alias.readline()
     alias_to_artist = dict()
@@ -25,16 +39,14 @@ def get_alias_to_name(artist_alias: str) -> dict:
     return alias_to_artist
 
 
-def replace_alias(artist_alias: str, user_artist_data: str, result_data: str):
-    _ = 0
-    alias_to_artist = get_alias_to_name(artist_alias)
+def _replace_alias(artist_alias: str, user_artist_data: str, result_data: str):
+    alias_to_artist = _get_alias_to_name(artist_alias)
     user_artist = open(user_artist_data)
     lines = []
     line = user_artist.readline()
     while line:
         line_content = line.split(' ')
         if int(line_content[1]) in alias_to_artist.keys():
-            _ += 1
             line_content[1] = alias_to_artist[int(line_content[1])]
             line_content = " ".join(str(x) for x in line_content)
             lines.append(line_content)
@@ -47,4 +59,3 @@ def replace_alias(artist_alias: str, user_artist_data: str, result_data: str):
     for line in lines:
         result.write(line)
     result.close()
-    print(_)
