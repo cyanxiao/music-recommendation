07fd4f29ec10fc031cfd4055c0c4996bec35e0d2
1170300304 <cxz2851151@163.com>
2020-10-27 18:35:17 +0800

    feat: add a .py to handle data for converting to preference. fix: the adjacent matrix need 820G, now I am concerning the possibility to use adjacent table.

---
+++ data_preprocess/data_convert.py
@@ -0,0 +1,225 @@
+import pandas as pd
+import numpy as np
+import gc
+
+
+class Node(object):
+    def __init__(self, char: str):
+        self.char = char
+        self.child = []
+        self.value = 0
+
+
+class TrieTree(object):
+    def __init__(self):
+        self.root = Node("")
+
+    def find(self, num: str, add: bool = True) -> bool:
+        ptr = self.root
+        for i in range(len(num)):
+            match = False
+            # in child
+            for nd in ptr.child:
+                if nd.char.__eq__(num[i]):
+                    match = True
+                    ptr = nd
+                    if i == len(num) - 1:
+                        if ptr.value == 1:
+                            return True
+                        else:
+                            if add:
+                                ptr.value = 1
+                            return False
+
+            if not match and not add:
+                return False
+            if not match:
+                d = Node(num[i])
+                ptr.child.append(d)
+                if i == len(num) - 1:
+                    d.value = 1
+                ptr = d
+        return False
+
+    def print_t(self):
+        def dfs(nd):
+            print("#")
+            print(nd.char)
+            print(nd.value)
+            print("##")
+            for d in nd.child:
+                dfs(d)
+        dfs(self.root)
+
+
+def convert2preference(user_artist_data: str, output: str, save: bool = True,
+                       data_sep: str = " ", result_sep: str = ",") -> np.ndarray:
+    """
+    把用户-艺术家-播放次数 表格转换成偏好矩阵。
+    :param data_sep: 数据集分隔符。
+    :param result_sep: 结果分隔符。
+    :param save: 是否保存映射和矩阵为文件。
+    :param user_artist_data:用户-艺术家-播放次数 列表的路径//名字，csv形式。
+    :param output: 偏好矩阵、用户映射、艺术家映射输出目录，以//或者\\结尾，依照具体的系统而用户自定。
+    :return: 返回偏好矩阵
+    """
+    f = open(user_artist_data, encoding="utf-8")
+    user_list = []
+    artist_list = []
+    trie = TrieTree()
+    art_dict = dict()
+    user_pref = dict()
+    first = True
+    print("reading data ... ...")
+    cnt = 0
+    last_user = -1
+    for line in f:
+        if first:
+            first = False
+            continue
+        args = line.strip().split(data_sep)
+        user = int(args[0])
+        artist = int(args[1])
+        num = int(args[2])
+        if user == last_user:
+            user_pref[user].append((artist, num))
+        else:
+            last_user = user
+            user_list.append(user)
+            user_pref[user] = []
+        # if not trie.find(args[1]):
+        if artist not in art_dict.keys():
+            art_dict[artist] = len(artist_list)
+            artist_list.append(artist_list)
+        cnt = cnt + 1
+        if cnt % 100000 == 0:
+            print("# " + str(cnt) + " was read")
+
+    f.close()
+    del f
+    del trie
+    gc.collect()
+    print("# " + str(cnt) + " was read")
+    print()
+    print("# Users Count:")
+    print(str(len(user_list)))
+    print("# Artists Count:")
+    print(str(len(art_dict.keys())))
+    print()
+    print("# generate preference ... ...")
+
+    # generate preference
+    cnt = 0
+    preference = []
+    artist_line = []
+    for j in range(len(artist_list)):
+        artist_line.append(-1)
+    for i in range(len(user_list)):
+        a = artist_line.copy()
+        lst = user_pref[user_list[i]]
+        for j in range(len(lst)):
+            a[art_dict[lst[j][0]]] = lst[j][1]
+        preference.append(a)
+        cnt = cnt + 1
+        if cnt % 1000 == 0:
+            print("# " + str(cnt) + " users was processed")
+
+    print("# " + str(cnt) + " users was processed")
+    print()
+    if save:
+        preference = np.array(preference)
+        save_matrix(preference, output + "preference.csv", sep=result_sep)
+        save_list_dict(user_list, output + "user_dict.csv", sep=result_sep)
+        save_list_dict(artist_list, output + "artist_dict.csv", sep=result_sep)
+
+    return np.array(preference, dtype='float64')
+
+
+def save_list_dict(mp: list, path: str, sep: str = ","):
+    """
+    Save the 1-1 dict for type int-int as a list.
+    :param sep: the separator of the file, default ",".
+    :param mp: the map that requires to save.
+    :param path: the path + name of the file.
+    :return: none.
+    """
+    lst = []
+    for i in range(len(mp)):
+        lst.append(str(i) + sep + str(mp[i]))
+    f = open(path, mode="w", encoding="utf-8")
+    f.write("\n".join(s for s in lst))
+    f.flush()
+    f.close()
+
+
+def save_dict(mp: dict, path: str, sep: str = ","):
+    """
+    Save the 1-1 dict for type int-int.
+    :param sep: the separator of the file, default ",".
+    :param mp: the map that requires to save.
+    :param path: the path + name of the file.
+    :return: none.
+    """
+    lst = []
+    for key in mp.keys():
+        lst.append(str(key) + sep + str(mp[key]))
+    f = open(path, mode="w", encoding="utf-8")
+    f.write("\n".join(s for s in lst))
+    f.flush()
+    f.close()
+
+
+def read_dict(path: str, sep: str = ","):
+    """
+    Read 1-1 dict with type int-int.
+    :param sep: the separator of the file, default ','.
+    :param path: the path + name of the stored position.
+    :return: the dict.
+    """
+    f = open(path, encoding="utf-8")
+    dic = {}
+    for line in f:
+        args = line.strip().split(sep)
+        dic[int(args[0])] = int(args[1])
+    return dic
+
+
+def save_matrix(matrix: np.ndarray, path: str, sep: str = ","):
+    """
+    Save a float64 matrix to file.
+    :param matrix: the saving matrix.
+    :param path: path + name.
+    :param sep: the separator of the file, default ','.
+    :return: null.
+    """
+    sb = []
+    for i in range(len(matrix)):
+        sb1 = []
+        for j in range(len(matrix[i])):
+            sb1.append(str(matrix[i][j]))
+        sb.append(sep.join(s for s in sb1))
+    f = open(path, mode="w", encoding="utf-8")
+    f.write("\n".join(s for s in sb))
+    f.flush()
+    f.close()
+
+
+def read_matrix(path: str, sep: str = ",") -> np.ndarray:
+    """
+    Read a matrix.
+    :param path:
+    :param sep:
+    :return:
+    """
+    f = open(path, encoding='utf-8')
+    mtxl = []
+    for line in f:
+        nums = line.strip().split(sep)
+        ll = []
+        for num in nums:
+            ll.append(float(num))
+        mtxl.append(ll)
+    return np.array(mtxl, dtype="float64")
+
+
+
--- mat_process/optimal_methods.py
+++ mat_process/optimal_methods.py
@@ -2,13 +2,22 @@ import numpy as np
 import mat_process.uv_decomposition as uvd
 
 
+def rmse(user_preference: np.ndarray, estimate: np.ndarray) -> np.float64:
+    return np.sqrt(np.sum((user_preference - estimate) ** 2) / np.size(user_preference))
+
+
+def rmse_delta(delta: np.ndarray) -> np.float64:
+    return np.sqrt(np.sum(delta ** 2) / np.size(delta))
+
+
 # gradient descend
 def gradient_desc_uv(user_preference: np.ndarray, latent_factor: int = 1,
-                     max_iter: int = 100, step: float = 0.1, lamda: float = 0,
-                     least: float = 0.1) -> np.ndarray:
+                     max_iter: int = 100, step: float = 0.01, lamda: float = 0,
+                     least: float = 0.001, print_msg: bool = False) -> np.ndarray:
     """
     UV decomposition with gradient descend methods.
-    :param least: the least change of each iterate.
+    :param print_msg: if should print rmse in each iteration
+    :param least: the least change of each iteration.
     :param lamda: the format term.
     :param step: the learning rate.
     :param max_iter: maximum iterate times.
@@ -18,6 +27,11 @@ def gradient_desc_uv(user_preference: np.ndarray, latent_factor: int = 1,
     """
     u, v = uvd.decompose(user_preference, latent_factor)
     last = uvd.get_rmse(u, v, user_preference)
+    optimal = last
+    opt_u = u.copy()
+    opt_v = v.copy()
+    if print_msg:
+        print("rmse 0: " + str(last))
     o = user_preference.copy()
     for i in range(max_iter):
         e = u.dot(v)
@@ -27,12 +41,20 @@ def gradient_desc_uv(user_preference: np.ndarray, latent_factor: int = 1,
                     o[j][b] = e[j][b]
         k = o - e  # k is the delta of user preference and UV
         u = u + step * (k.dot(v.T) - lamda * u)
-        v = v + step * (k.dot(u.T) - lamda * v)
+        v = v + step * (u.T.dot(k) - lamda * v)
         now = uvd.get_rmse(u, v, user_preference)
+        if print_msg:
+            print("rmse" + str(i + 1) + ": " + str(now))
+        if now < optimal:
+            optimal = now
+            opt_u = u.copy()
+            opt_v = v.copy()
         if abs(now - last) < least:
             break
         last = now
-    return u.dot(v)
+    print(opt_u)
+    print(opt_v)
+    return opt_u.dot(opt_v)
 
 
 def newton_uv(user_preference: np.ndarray, latent_factor: int = 1) -> np.ndarray:
