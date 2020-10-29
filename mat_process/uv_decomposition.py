2ff2bb9785969a26b2447830a1575ce91d4db959
RealTonyShaw <tonyshaw123@hotmail.com>
2020-10-29 16:42:36 +0800

    feat: add sparse mat decomposition

--- mat_process/uv_decomposition.py
+++ mat_process/uv_decomposition.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.sparse as ss
 
 
 def decompose(user_preference: np.ndarray, latent_factor_num: int = 1) -> (np.ndarray, np.ndarray):
@@ -11,6 +12,22 @@ def decompose(user_preference: np.ndarray, latent_factor_num: int = 1) -> (np.nd
     rows_num, column_num = user_preference.shape
     not_nan_elements_num = np.count_nonzero(~np.isnan(user_preference))
     avg = np.nansum(user_preference) / not_nan_elements_num
+    init_element = np.sqrt(avg / latent_factor_num)
+    u_init, v_init = np.full((rows_num, latent_factor_num), init_element), \
+                     np.full((latent_factor_num, column_num), init_element)
+    return u_init, v_init
+
+
+def sparse_mat_decompose(user_preference: ss.csr_matrix, latent_factor_num: int = 1) -> (ss.csr_matrix, ss.csr_matrix):
+    """
+    分解稀疏的用户偏好矩阵至两个初始化矩阵 U 和 V，U 的列数和 V 的行数为 latent_factor_num
+    :param user_preference: 分解用户偏好矩阵
+    :param latent_factor_num: U 的列数和 V 的行数
+    :return: U, V
+    """
+    rows_num, column_num = user_preference.shape
+    not_nan_elements_num = user_preference.count_nonzero()
+    avg = user_preference.sum() / not_nan_elements_num
     init_element = np.sqrt(avg / latent_factor_num)
     u_init, v_init = np.full((rows_num, latent_factor_num), init_element), \
                      np.full((latent_factor_num, column_num), init_element)
